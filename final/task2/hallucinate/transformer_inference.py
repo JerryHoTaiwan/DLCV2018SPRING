cd176a816aa912cf0c89c708f4caf97e1cdc74ac
CheNgH826 <lilioo826@gmail.com>
2018-06-27 14:31:33 +0800

    finish mean version

--- final/task2/hallucinate/transformer_inference.py
+++ final/task2/hallucinate/transformer_inference.py
@@ -4,6 +4,7 @@ import scipy.misc as misc
 import numpy as np
 import itertools
 import os
+import random
 
 n_cluster = 20
 # def get_pair_from_clusters(centroids):
@@ -13,8 +14,11 @@ n_cluster = 20
 #     return np.array(pair_list)
 
 if __name__ == '__main__':
-    mode = 'val'
+    mode = 'train'
+    submode = 'mean'
     checkpoint_path = 'model/transformer_78.pth'
+    checkpoint_path_mean = 'model/transformer_31_mean.pth'
+    
     classes_centroids_path = 'classes_centroids.npy'
     novel_classes_path = '../data/task2-dataset/novel/'
     ResNet_model_path = 'Res_trained_39.pkl'
@@ -34,12 +38,14 @@ if __name__ == '__main__':
                 img = misc.imread(novel_classes_path+novel_class_name+'/train/00%d.png'%(i+1))
                 novel_imgs.append(img)
         novel_imgs_tensor = torch.Tensor(novel_imgs).view(-1, 3, 32, 32)
-        print(novel_imgs_tensor.shape)
+        # print(novel_imgs_tensor.shape)
 
         _, novel_features = my_classifier(novel_imgs_tensor.cuda())
-        print(novel_features.shape)
-
-        checkpoint = torch.load(checkpoint_path)
+        # print(novel_features.shape)
+        if submode == 'mean':
+            checkpoint = torch.load(checkpoint_path_mean)
+        else:
+            checkpoint = torch.load(checkpoint_path)
         transformer = Transformer(feature_dim)
         transformer.load_state_dict(checkpoint['state_dict'])
         transformer.eval().cuda()
@@ -50,11 +56,22 @@ if __name__ == '__main__':
             novel_classi_aug_feat_pool = []
             for novel_raw_feature in novel_features[i:i+n_shot]:
                 novel_classi_aug_feat_pool.append(novel_raw_feature.cpu().detach().numpy())
-                for classi in classes_centroids_np[:10]:
-                    for pair in itertools.permutations(classi, 2):
-                        novel_transformed = transformer(novel_raw_feature.cuda(), torch.tensor(pair[0]).cuda(), torch.tensor(pair[1]).cuda()).cpu().detach().numpy()
-                        novel_classi_aug_feat_pool.append(novel_transformed)
-            np.save('npy/'+novel_class_list[i]+'.npy', np.array(novel_classi_aug_feat_pool))
+                for classi in classes_centroids_np:
+                    if submode == 'mean':
+                        classi_centroid_mean = np.mean(classi, 0)
+                        classi_shuffled = classi[:]
+                        random.shuffle(classi_shuffled)
+                        for classi_centroid in classi_shuffled[:5]:
+                            novel_transformed = transformer(novel_raw_feature.cuda(), torch.tensor(classi_centroid_mean).cuda(), torch.tensor(classi_centroid).cuda()).cpu().detach().numpy()
+                            novel_classi_aug_feat_pool.append(novel_transformed)
+                    else:
+                        pairs = itertools.permutations(classi, 2)
+                        pairs_shuffled = pairs[:]
+                        random.shuffle(pairs_shuffled)
+                        for pair in pairs_shuffled[:5]:
+                            novel_transformed = transformer(novel_raw_feature.cuda(), torch.tensor(pair[0]).cuda(), torch.tensor(pair[1]).cuda()).cpu().detach().numpy()
+                            novel_classi_aug_feat_pool.append(novel_transformed)
+            np.save('npy/'+novel_class_list[i]+'_mean.npy', np.array(novel_classi_aug_feat_pool))
             print(np.array(novel_classi_aug_feat_pool).shape)
     
     elif mode == 'val':
--- final/task2/hallucinate/transformer_training.py
+++ final/task2/hallucinate/transformer_training.py
@@ -7,9 +7,32 @@ import numpy as np
 import torch.nn as nn
 np.random.seed(2)
 
+def collect_mean_quaruplets(class1_centroids, class2_centroids):
+    n_cluster = class1_centroids.shape[0]
+    feature_dim = class1_centroids.shape[1]
+
+    class1_mean = np.mean(class1_centroids, 0)
+    class2_mean = np.mean(class2_centroids, 0)
+    print(class2_mean.shape)
+
+    class1_vectors = np.zeros((n_cluster, feature_dim))
+    class2_vectors = np.zeros((n_cluster, feature_dim))
+
+    for i in range(n_cluster):
+        class1_vectors[i] = class1_centroids[i]-class1_mean
+        class2_vectors[i] = class2_centroids[i]-class2_mean
+
+    quaruplets = []
+    for i in range(n_cluster):
+        for j in range(n_cluster):
+            if 1-spatial.distance.cosine(class1_vectors[i], class2_vectors[j]) > 0:
+                quaruplets.append((i, j))
+    return quaruplets
+
 def collect_quaruplets(class1_centroids, class2_centroids):
     n_cluster = class1_centroids.shape[0]
     feature_dim = class1_centroids.shape[1]
+
     class1_inner_vector = np.zeros((n_cluster, n_cluster, feature_dim))
     class2_inner_vector = np.zeros((n_cluster, n_cluster, feature_dim))
     
@@ -32,46 +55,78 @@ def get_transform_pair_seq(n_cluster):
     return list(itertools.permutations(range(n_cluster), 2))
 
 def train(transformer, classifier, optimizer, class1_centroids, class2_centroids, class1_label, n_epoch=20, batch_size=64, wt=10, save_model_path='model/'):
+    mode = 'mean'
     transformer.train().cuda()
     classifier.eval()
     loss_cls = nn.CrossEntropyLoss()
     loss_mse = nn.MSELoss()
 
     n_cluster = len(class1_centroids)
-    quaruplet_list = collect_quaruplets(class1_centroids, class2_centroids)
-    class1_shuffled_centroids = np.random.shuffle(class1_centroids)
-    class2_shuffled_centroids = np.random.shuffle(class2_centroids)
+    if mode == 'mean':
+        quaruplet_list = collect_mean_quaruplets(class1_centroids, class2_centroids)
+        class1_mean = np.mean(class1_centroids, 0)
+        class2_mean = np.mean(class2_centroids, 0)
 
-    for epoch in range(n_epoch):
-        for i in range(0, len(quaruplet_list), batch_size):
-            A, B, C, D = [], [], [], []
-            for j in range(i, min(i+batch_size, len(quaruplet_list))):
-                A.append(torch.tensor(class1_centroids[quaruplet_list[j][0]]))
-                B.append(torch.tensor(class1_centroids[quaruplet_list[j][1]]))
-                C.append(torch.tensor(class1_centroids[quaruplet_list[j][2]]))
-                D.append(torch.tensor(class1_centroids[quaruplet_list[j][3]]))
-            A_tensor = torch.stack(A).cuda()
-            B_tensor = torch.stack(B).cuda()
-            C_tensor = torch.stack(C).cuda()
-            D_tensor = torch.stack(D).cuda()
-            # print(A_tensor.shape)
+        for epoch in range(n_epoch):
+            for i in range(0, len(quaruplet_list), batch_size):
+                A, B, C, D = [], [], [], []
+                for j in range(i, min(i+batch_size, len(quaruplet_list))):
+                    A.append(torch.tensor(class1_mean))
+                    B.append(torch.tensor(class1_centroids[quaruplet_list[j][0]]))
+                    C.append(torch.tensor(class2_mean))
+                    D.append(torch.tensor(class2_centroids[quaruplet_list[j][1]]))
+                A_tensor = torch.stack(A).cuda()
+                B_tensor = torch.stack(B).cuda()
+                C_tensor = torch.stack(C).cuda()
+                D_tensor = torch.stack(D).cuda()
+                # print(A_tensor.shape)
 
-            B_hat = transformer(A_tensor, C_tensor, D_tensor)
-            loss_mse_val = loss_mse(B_hat, B_tensor).cpu()
-            class_prediction = classifier.classifier(B_hat).cpu()
-            class_truth = torch.empty(B_hat.shape[0], dtype=torch.long).fill_(class1_label).cpu()
-            loss_cls_val = loss_cls(class_prediction, class_truth)
-            loss = loss_mse_val + wt*loss_cls_val
+                B_hat = transformer(A_tensor, C_tensor, D_tensor)
+                loss_mse_val = loss_mse(B_hat, B_tensor).cpu()
+                class_prediction = classifier.classifier(B_hat).cpu()
+                class_truth = torch.empty(B_hat.shape[0], dtype=torch.long).fill_(class1_label).cpu()
+                loss_cls_val = loss_cls(class_prediction, class_truth)
+                loss = loss_mse_val + wt*loss_cls_val
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
 
-        print('[epoch %d/%d] loss: %f, mse_loss: %f, cls_loss: %f'%(epoch+1, n_epoch, loss.item(), loss_mse_val.item(), loss_cls_val.item()))
-    torch.save({'state_dict':transformer.state_dict(), 'optimizer': optimizer.state_dict()}, save_model_path+'transformer_%d.pth'%(class1_label))
+            print('[epoch %d/%d] loss: %f, mse_loss: %f, cls_loss: %f'%(epoch+1, n_epoch, loss.item(), loss_mse_val.item(), loss_cls_val.item()))
+        torch.save({'state_dict':transformer.state_dict(), 'optimizer': optimizer.state_dict()}, save_model_path+'transformer_%d_mean.pth'%(class1_label))
+
+    else:
+        quaruplet_list = collect_quaruplets(class1_centroids, class2_centroids)
+
+        for epoch in range(n_epoch):
+            for i in range(0, len(quaruplet_list), batch_size):
+                A, B, C, D = [], [], [], []
+                for j in range(i, min(i+batch_size, len(quaruplet_list))):
+                    A.append(torch.tensor(class1_centroids[quaruplet_list[j][0]]))
+                    B.append(torch.tensor(class1_centroids[quaruplet_list[j][1]]))
+                    C.append(torch.tensor(class2_centroids[quaruplet_list[j][2]]))
+                    D.append(torch.tensor(class2_centroids[quaruplet_list[j][3]]))
+                A_tensor = torch.stack(A).cuda()
+                B_tensor = torch.stack(B).cuda()
+                C_tensor = torch.stack(C).cuda()
+                D_tensor = torch.stack(D).cuda()
+                # print(A_tensor.shape)
+
+                B_hat = transformer(A_tensor, C_tensor, D_tensor)
+                loss_mse_val = loss_mse(B_hat, B_tensor).cpu()
+                class_prediction = classifier.classifier(B_hat).cpu()
+                class_truth = torch.empty(B_hat.shape[0], dtype=torch.long).fill_(class1_label).cpu()
+                loss_cls_val = loss_cls(class_prediction, class_truth)
+                loss = loss_mse_val + wt*loss_cls_val
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            print('[epoch %d/%d] loss: %f, mse_loss: %f, cls_loss: %f'%(epoch+1, n_epoch, loss.item(), loss_mse_val.item(), loss_cls_val.item()))
+        torch.save({'state_dict':transformer.state_dict(), 'optimizer': optimizer.state_dict()}, save_model_path+'transformer_%d.pth'%(class1_label))
 
 if __name__ == '__main__':
-    n_cluster = 10
     feature_dim = 512
     n_classes = 80
     
